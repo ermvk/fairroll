704544926fc1b4c343a464a189b2eb172583ab63
mvrduk <mardukh.pro@gmail.com>
2026-06-20 22:36:05 +0700

    chore: apply gitignore

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"context"
+	"fairroll/pkg/config"
 	"fairroll/pkg/logger"
+	"log"
 	"time"
 )
 
 func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-
 	defer cancel()
 
-	LoggerCfg := logger.Config{
-		Loglevel: cfg.L
+	cfg, err := config.LoadConfig(ctx)
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
 	}
+
+	LoggerCfg := logger.Config{
+		Loglevel: cfg.Logger.Level,
+		Format:   cfg.Logger.Format,
+		OutPut:   "stdout",
+	}
+	err = logger.Init(LoggerCfg)
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
+	defer logger.Sync()
+
+	logger.Info(ctx, "Starting Auth Service",
+		"name", cfg.Service.Name,
+		"version", cfg.Service.Version,
+		"port", cfg.Service.Port,
+		"environment", cfg.Environment,
+	)
 }
